f33d5504ec10c1bf3325391f3210d4a95f38ae50
Hugo Cardenas <SeeKNayWen_END@outlook.com>
2024-03-29 20:22:15 -0600

    Add project files.

---
+++ DataAccess_Layer/DB_TestSolContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataAccess_Layer
+{
+    public partial class DB_TestSolContext : DbContext
+    {
+        public DB_TestSolContext()
+        {
+        }
+
+        public DB_TestSolContext(DbContextOptions<DB_TestSolContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Empleado> Empleados { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DB_TestSol.mssql.somee.com; Database= DB_TestSol; TrustServerCertificate=True; User ID=Yuugo12_SQLLogin_1; Password=9y92gt8vq8;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.HasKey(e => e.IdEmpleado);
+
+                entity.ToTable("Empleado");
+
+                entity.Property(e => e.ApellidoMaterno)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ApellidoPaterno)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Area)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FechaNacimiento).HasColumnType("date");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sueldo)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DataAccess_Layer/Empleado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess_Layer
+{
+    public partial class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string ApellidoPaterno { get; set; } = null!;
+        public string ApellidoMaterno { get; set; } = null!;
+        public string Area { get; set; } = null!;
+        public DateTime? FechaNacimiento { get; set; }
+        public string Sueldo { get; set; } = null!;
+    }
+}
